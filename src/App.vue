fa29623de473bb885459fcc4df691a15b81a541f
Kffhi <13979215061@163.com>
2019-04-03 16:43:21 +0800

    点击翻页

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,23 @@
     </div>
 </template>
 <script>
-    document.addEventListener('DOMContentLoader', () => {
-        const html = document.querySelector('html')
-        let fontSize = window.innerWidth / 10
-        fontSize = fontSize > 50 ? 50 : fontSize
-        html.style.fontSize = fontSize + 'px'
-    })
+    export default {
+        name: 'App'
+    }
+    document.addEventListener("DOMContentLoaded", setFontSize);
+    window.addEventListener('resize', setFontSize)
+    function setFontSize() {
+        const html = document.querySelector("html");
+        let fontSize = window.innerWidth / 10;
+        fontSize = fontSize > 50 ? 50 : fontSize;
+        html.style.fontSize = fontSize + "px";
+    }
+    // document.addEventListener('DOMContentLoader', () => {
+    //     const html = document.querySelector('html');
+    //     let fontSize = window.innerWidth / 10;
+    //     fontSize = fontSize > 50 ? 50 : fontSize;
+    //     html.style.fontSize = fontSize + 'px'
+    // })
 </script>
 <style>
 </style>
--- src/Ebook.vue
+++ src/Ebook.vue
@@ -1,18 +1,35 @@
 <template>
-<div class="ebook">
-    <div class="read-wrapper">
-        <div id="read"></div>
+    <div class="ebook">
+        <div class="read-wrapper">
+            <div id="read">
+                <div class="mask">
+                    <div class="left" @click="prevPage"></div>
+                    <div class="center"></div>
+                    <div class="right" @click="nextPage"></div>
+                </div>
+            </div>
+        </div>
     </div>
-</div>
 </template>
 
 <script>
     import Epub from 'epubjs'
+
     const DOWNLOAD_URL = '/2018_Book_AgileProcessesInSoftwareEngine.epub'
     global.ePub = Epub
     export default {
         name: "Ebook",
         methods: {
+            prevPage() {
+                if (this.rendition) {
+                    this.rendition.prev()
+                }
+            },
+            nextPage() {
+                if (this.rendition) {
+                    this.rendition.next()
+                }
+            },
             // 电子书的解析和渲染
             showEpub() {
                 // 生成Book
@@ -34,5 +51,32 @@
 </script>
 
 <style lang="scss" scoped>
-@import "./assets/styles/global";
+    @import "./assets/styles/global";
+
+    .ebook {
+        position: relative;
+        .read-wrapper {
+            .mask {
+                position: absolute;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+                z-index: 100;
+                display: flex;
+
+                .left {
+                    flex: 0 0 px2rem(100);
+                }
+
+                .center {
+                    flex: 1;
+                }
+
+                .right {
+                    flex: 0 0 px2rem(100);
+                }
+            }
+        }
+    }
 </style>

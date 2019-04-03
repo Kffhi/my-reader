91692a6a14411d711af4da19a3c983a8e738e35f
Kffhi <13979215061@163.com>
2019-04-03 15:16:49 +0800

    viewport、rem、reset配置

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/styles/icon.css'
+import './assets/styles/global.scss'
 
 Vue.config.productionTip = false
 
